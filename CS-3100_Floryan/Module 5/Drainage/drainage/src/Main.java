b9fdf51250d81e4f03197e13ee4c2b34ab8a1891
StormLok953 <tyler.ys.kim@gmail.com>
2022-10-27 14:39:47 -0400

    stuff

--- CS-3100_Floryan/Module 5/Drainage/drainage/src/Main.java
+++ CS-3100_Floryan/Module 5/Drainage/drainage/src/Main.java
@@ -1,7 +1,76 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    
+    static String testCase1 = "C:/Users/tyler/dev/UVA-CS/CS-3100_Floryan/Module 5/Drainage/test-cases/test-case-1.txt";
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        ArrayList<String> parsedString = new ArrayList<>();
+        parsedString = getTestCase(testCase1);
+
+        //submission purposes
+//        Scanner input = new Scanner(System.in);
+//        while(input.hasNextLine()) {
+//            String data = input.nextLine();
+//            parsedString.add(data);
+//        }
+
+        int size = Integer.parseInt(parsedString.get(0));
+        parsedString.remove(0);
+
+        for(int i = 0; i < size; i++) {
+            ArrayList<String> testCase = grabTestCase(parsedString);
+
+
+        }
+
+    }
+
+    //helper
+    public static ArrayList<String> grabTestCase(ArrayList<String> parsedString) {
+        ArrayList<String> arr = new ArrayList<>();
+        String[] data = parsedString.get(0).split(" ");
+        int n = Integer.parseInt(data[1]);
+
+        for(int i = 0; i <= n; i++) {
+            arr.add(parsedString.get(0));
+            parsedString.remove(0);
+        }
+
+        return arr;
+    }
+
+    
+
+    //testing
+    public static ArrayList<String> getTestCase(String filepath) {
+        File file = new File(filepath);
+        ArrayList<String> arr = new ArrayList<>();
+        try {
+            Scanner input = new Scanner(file);
+            while(input.hasNextLine()) {
+                String data = input.nextLine();
+                arr.add(data);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return arr;
+    }
+
+    public static void printArrayList(ArrayList<String> arr){
+
+        for(String s: arr) {
+            System.out.println(s);
+        }
+
+        System.out.println();
+
     }
 
 }
