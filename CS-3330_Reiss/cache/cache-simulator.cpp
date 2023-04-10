55f51b4a2ade92e2b5efb5ada3d1005482c4cdac
Tazering <tyler.ys.kim@gmail.com>
2023-04-09 20:43:30 -0400

    stuff

--- CS-3330_Reiss/cache/cache-simulator.cpp
+++ CS-3330_Reiss/cache/cache-simulator.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 int main() {
+    /* int argc: length of arguments
+       char** argv: command line arguments
+
+       argv[1]: sets
+       argv[2]: ways
+       argv[3]: bytes per block
+    */
     
+
+
     return 0;
 }
+
