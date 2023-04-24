8abaeefab43fe60d5653e84f4d78ba5966f70105
Tazering <tyler.ys.kim@gmail.com>
2023-04-24 01:44:00 -0400

    stuff

---
+++ CS-3710_Bloomfield/buffer-overflow/shellcode.s
@@ -0,0 +1,23 @@
+; shellcode.s
+
+	global shellcode
+	
+	section .text
+
+shellcode:
+	section .text
+	mysyscall:
+		jmp afterString
+	string:
+		db "Tyler Kim, your grade on this assignment is an A", 0x0a
+	afterString:
+		xor rax, rax ; zero out %rax
+		mov al, 1 ; set code write for when we initiate a system call
+		mov rdi, rax
+		lea rsi, [rel string] ; move string to first parameter
+		xor rdx, rdx ; zero out %rdx
+		mov dl, 36
+		syscall	
+		mov al, 60
+		xor rdi, rdi
+		syscall
---
+++ CS-3710_Bloomfield/buffer-overflow/stack_addr.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+	char buffer[256];
+	printf ("%.8lx\n",(unsigned long)buffer);
+}
