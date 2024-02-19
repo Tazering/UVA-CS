1e5a5f61fb347fbc15f5e14a90206cf1d98261cb
Tazering <tyler.ys.kim@gmail.com>
2024-02-19 17:44:27 -0500

    stuff

---
+++ CS-4414_Lin/p1-kernel/p1 exp4b/exp4b/src/kernel.c
@@ -0,0 +1,89 @@
+#include "printf.h"
+#include "utils.h"
+#include "timer.h"
+#include "irq.h"
+#include "fork.h"
+#include "sched.h"
+#include "mini_uart.h"
+
+//processes
+void process(char *array)
+{
+	while (1) {
+		for (int i = 0; i < 5; i++){
+			uart_send(array[i]);
+			delay(5000000);
+		}
+	}
+}
+
+void process2(char *array)
+{
+	while (1) {
+		for (int i = 0; i < 5; i++){
+			uart_send(array[i]);
+			delay(5000000);
+		}
+	}
+}
+
+void process3(char *array)
+{
+	while (1) {
+		for (int i = 0; i < 5; i++){
+			uart_send(array[i]);
+			delay(5000000);
+		}
+	}
+}
+
+void process4(char *array)
+{
+	while (1) {
+		for (int i = 0; i < 5; i++){
+			uart_send(array[i]);
+			delay(5000000);
+		}
+	}
+}
+
+void kernel_main(void)
+{
+	uart_init();
+	init_printf(0, putc);
+
+	printf("kernel boots\n");
+
+	irq_vector_init();
+	generic_timer_init();
+	enable_interrupt_controller();
+	enable_irq();
+
+	//creates the processes
+	int res = copy_process((unsigned long)&process, (unsigned long)"12345"); // process 1
+	if (res != 0) {
+		printf("error while starting process 1");
+		return;
+	}
+	res = copy_process((unsigned long)&process2, (unsigned long)"abcde"); //process 2
+	if (res != 0) {
+		printf("error while starting process 2");
+		return;
+	}
+
+	res = copy_process((unsigned long)&process3, (unsigned long)"67890"); // process 3
+	if(res != 0) {
+		printf("error while starting process 3");
+		return;
+	}
+
+	res = copy_process((unsigned long)&process4, (unsigned long)"fghij"); // process 4
+	if(res != 0) {
+		printf("error while starting process 4");
+		return;
+	}
+
+	while (1){
+		schedule();
+	}	
+}
