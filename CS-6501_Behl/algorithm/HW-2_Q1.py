62a60f4ed63de0d09a9d355c25dd204fde4d6653
tkj9ep <tkj9ep@virginia.edu>
2025-03-27 19:11:38 -0400

    stuff

--- CS-6501_Behl/algorithm/HW-2_Q1.py
+++ CS-6501_Behl/algorithm/HW-2_Q1.py
@@ -7,14 +7,16 @@ def main(seed = 42):
     np.random.seed(seed)
 
     distribution_param = {} # stores the distribution parameters
-    distribution_param[0] = {"mean": np.array([[1], [-7]]), "covariance": np.array([[2, 0],[0, 100]])}
+    distribution_param[0] = {"mean": np.array([[1], [2]]), "covariance": np.array([[2, 0],[0, 100]])}
 
-    observations = simulate(plot = False) # get observations and plot
+    observations, means = simulate(plot = True) # get observations and plot
     distribution_param, a_list = extended_kalman_filter(distribution_param, observations)
 
-    plot_ekf_vs_ground_truth(observations, distribution_param)
+    convert_dict_to_latex(distribution_param, a_list)
 
-    # print(a_list)
+    plot_ekf_vs_ground_truth(means, distribution_param)
+
+    print(a_list)
 
 
     return 0
@@ -23,11 +25,13 @@ def main(seed = 42):
 def simulate(a = -1, iterations = 100, plot = True):
 
     observation = []
+    means = []
     
     # get the initial state and observation
     x_i = np.random.normal(1, 2)
     v = np.random.normal(0, 1/2) # noise
     y_i = get_observation(x_i, v) # initial observation
+    means.append(x_i)
 
     observation.append(y_i) # add first observation
 
@@ -39,16 +43,23 @@ def simulate(a = -1, iterations = 100, plot = True):
         y_i = get_observation(x_i, v)
 
         observation.append(y_i)
+        means.append(x_i)
     
     if plot:
-        plt.title("x_k vs y_k")
+        plt.title("X_k")
+        plt.plot(means)
+        plt.xlabel("Unit of Time")
+        plt.ylabel("X_k")
+        plt.show()
+
+        plt.title("Y_k")
         plt.plot(observation)
-        plt.xlabel("X_k")
+        plt.xlabel("Unit of Time")
         plt.ylabel("Y_k")
         plt.show()
     
 
-    return  observation
+    return observation, means
 
 # b) extended kalman filter
 def extended_kalman_filter(distribution_param, observations):
@@ -133,7 +144,7 @@ def get_observation(state, v = None, incorporate_noise = True):
 
     return y_k
 
-def plot_ekf_vs_ground_truth(observations, distribution_parameters):
+def plot_ekf_vs_ground_truth(means, distribution_parameters):
 
     mu_plus_sigma = []
     mu_minus_sigma = []
@@ -146,14 +157,39 @@ def plot_ekf_vs_ground_truth(observations, distribution_parameters):
         mu_minus_sigma.append(mu - sigma)
 
     plt.title("True vs. Estimated Values (a = -1)")
-    plt.plot(np.arange(100), observations, label = "Ground Truth")
+    plt.plot(np.arange(100), means, label = "Ground Truth")
     plt.plot(mu_plus_sigma, label = "mu + sigma")
     plt.plot(mu_minus_sigma, label = "mu - sigma")
     plt.xlabel("Time Units")
-    plt.ylabel("Observation")
+    plt.ylabel("State")
     plt.legend()
     plt.show()
 
     return None
+
+
+def convert_dict_to_latex(dictionary, a_list):
+    latex_command = "\\begin{center}\nTable \\ref{Results} has the mean, covariances, and a values.\\\\\n\\label{Results}\n\\begin{tabular}{| c | c | c | c |}\n\\hline"
+    latex_command = latex_command + "\nt & a & mean & covariance \\\\\n\\hline\\hline\n"
+
+    key_list = np.array([])
+    for key in dictionary.keys():
+        key_list = np.append(key_list, key)
+    
+    # key_list = np.flip(key_list)
+
+    for key in key_list:
+        val = dictionary[key]
+        mean = val["mean"][0][0]
+        covariance = val["covariance"][0, 0]
+        a = a_list[int(key)]
+
+        latex_command = latex_command + f"{int(key + 1)} & {a} & {mean} & {covariance}\\\\\n\\hline\n"
+
+    latex_command = latex_command + "\\end{tabular}\n\\end{center}"
+
+    print(latex_command)
+
+
 # call the main function
 main()
